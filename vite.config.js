18e486592f0dc4730608c036beae7ad96ac0ed85
Francisco Lopez <lopezf28@gmail.com>
2024-12-02 00:50:53 -0300

    actualizacion vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/universe-travel/',  // Nombre de tu repositorio
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  }
+  base: '/universe-travel/',
 })
